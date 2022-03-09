ab637d553d9b59dbb30f02eb8d6bc947082eb952
kobus <git@kobus.com>
2022-03-09 12:03:53 +0100

    add git ssh key

--- config/security/ssh-client.nix
+++ config/security/ssh-client.nix
@@ -8,53 +8,39 @@ in
 {
   primary-user.home-manager.programs.ssh = {
     enable = true;
-    extraOptionOverrides = optionalAttrs isDarwin {
-      IgnoreUnknown = "AddKeysToAgent,UseKeychain";
-    };
 
-    extraConfig = optionalString isDarwin ''
-      AddKeysToAgent yes
-      UseKeychain yes
-    '';
-
-    userKnownHostsFile =
-      if isDarwin
-      then "~/.ssh/known_hosts"
-      else "/secrets/openssh/client/${username}/known_hosts";
+    # userKnownHostsFile = "~/.ssh/known_hosts";
 
     # NOTE: lol this is awful, there's gotta be a better way to handle these.
     matchBlocks = {
-      "10.0.1.150" = {
-        hostname = "10.0.1.150";
-        user = "kobus";
-      } // optionalAttrs isDarwin {
-        identityFile = [ "~/.ssh/id_enigma" ];
-      } // optionalAttrs isLinux {
-        identityFile = [ "/secrets/openssh/client/${username}/id_enigma" ];
-      };
+      # "10.0.1.150" = {
+      #   hostname = "10.0.1.150";
+      #   user = "kobus";
+      # } // optionalAttrs isDarwin {
+      #   identityFile = [ "~/.ssh/id_enigma" ];
+      # } // optionalAttrs isLinux {
+      #   identityFile = [ "/secrets/openssh/client/${username}/id_enigma" ];
+      # };
 
       "github.com" = {
         hostname = "github.com";
         user = "git";
-      } // optionalAttrs isDarwin {
-        identityFile = [ "~/.ssh/id_github" ];
-      } // optionalAttrs isLinux {
-        identityFile = [ "/secrets/openssh/client/${username}/id_github" ];
+        identityFile = "~/.ssh/github_ed25519";
       };
 
-      "gitlab.com" = {
-        hostname = "gitlab.com";
-        user = "git";
-      } // optionalAttrs isDarwin {
-        identityFile = [ "~/.ssh/id_gitlab" ];
-      } // optionalAttrs isLinux { };
+      # "gitlab.com" = {
+      #   hostname = "gitlab.com";
+      #   user = "git";
+      # } // optionalAttrs isDarwin {
+      #   identityFile = [ "~/.ssh/id_gitlab" ];
+      # } // optionalAttrs isLinux { };
 
-      "build.stackage.org" = {
-        user = "curators";
-        hostname = "build.stackage.org";
-      } // optionalAttrs isDarwin {
-        identityFile = [ "~/.ssh/id_rsa_stackage" ];
-      } // optionalAttrs isLinux { };
+      # "build.stackage.org" = {
+      #   user = "curators";
+      #   hostname = "build.stackage.org";
+      # } // optionalAttrs isDarwin {
+      #   identityFile = [ "~/.ssh/id_rsa_stackage" ];
+      # } // optionalAttrs isLinux { };
     };
   };
 }
